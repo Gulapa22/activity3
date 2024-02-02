a6def2ef340d0a0fa9fd29a251799ebf85980558
jasper ingal <jasperingal093@gmail.com>
2024-02-02 15:10:53 +0800

    ingal

--- login.php
+++ login.php
@@ -60,7 +60,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>LogiN Page</title>
     <style>
         body {
             font-family: Arial, sans-serif;
